9df5f2ff83b8428bbae79ae8511bd34560b3a3de
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-05-03 22:05:50 -0700

    Initial Commit

---
+++ Audio Summarization/Audio_SummarizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_SummarizationApp.swift
+//  Audio Summarization
+//
+//  Created by Sami Bajwa on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Audio_SummarizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
