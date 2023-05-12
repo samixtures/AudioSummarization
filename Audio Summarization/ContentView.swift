b71cdbd99428c41bb9167a0928a7b84c47e4cc01
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-05-12 16:41:57 -0700

    Properly gets Youtube link's ID

--- Audio Summarization/ContentView.swift
+++ Audio Summarization/ContentView.swift
@@ -7,24 +7,34 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     
     @State private var youtubeLink = ""
     @State private var summaryText = ""
     
     var body: some View {
-        VStack {
-            TextField("Enter Youtube Link", text: $youtubeLink)
-        }
-        .textFieldStyle(.roundedBorder)
-        .padding()
         
-        Button(action: {
-            summaryText = "Here's the summary of the audio"
-        }) {
-            Text("Transcribe Audio")
+        VStack {
+            
+            TextField("Enter Youtube Link", text: $youtubeLink)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            
+            Button(action: {
+                summaryText = youtubeLink.getYouTubeVideoID() ?? "incorrect link"
+                print("summaryTest says \(summaryText)")
+            })
+            {
+                Text("Transcribe Audio")
+            }
+            .buttonStyle(.bordered)
+            
+            ScrollView {
+                Text(summaryText)
+            }
+            
         }
-        .buttonStyle(.bordered)
     }
 }
 
@@ -33,3 +43,25 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+// Function
+extension String {
+    func getYouTubeVideoID() -> String? {
+        let pattern = #"^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([a-zA-Z0-9_-]{11})"#
+        // Regular expression pattern for matching YouTube video URLs
+        guard let regex = try? NSRegularExpression(pattern: pattern) else { return nil }
+        // Create regular expression object
+        let range = NSRange(location: 0, length: utf16.count)
+        // Create range for search
+        let match = regex.firstMatch(in: self, range: range)
+        // Search for match
+        guard let match = match, let videoIDRange = Range(match.range(at: 1), in: self) else { return nil }
+        // Extract video ID range from match
+        let videoID = String(self[videoIDRange])
+        // Extract video ID string from URL using range
+        return videoID
+    }
+}
+
